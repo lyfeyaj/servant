f3334e6057dd09799b5b61231af8e0a98932c509
lyfeyaj <lyfeyaj@gmail.com>
2014-03-14 17:12:22 +0800

    update sails version to 0.10.0-rc4

--- server/api/responses/badRequest.js
+++ server/api/responses/badRequest.js
@@ -1,32 +1,29 @@
 /**
- * Default 400 (Bad Request) handler
+ * 400 (Bad Request) Handler
  *
- * Sails will automatically respond using this middleware when a blueprint is requested
- * with missing or invalid parameters
- * (e.g. `POST /user` was used to create a user, but required parameters were missing)
- *
- * This middleware can also be invoked manually from a controller or policy:
- * res.badRequest( [validationErrors], [redirectTo] )
- *
- *
+ * Usage:
+ * return res.badRequest(
+ *   'Please choose a valid `password` (6-12 characters)',
+ *   '/trial/signup'
+ * );
+ * 
  * @param {Array|Object|String} validationErrors
  *      optional errors
  *      usually an array of validation errors from the ORM
  *
  * @param {String} redirectTo
  *      optional URL
- *      (absolute or relative, e.g. google.com/foo or /bar/baz) 
+ *      (absolute or relative, e.g. google.com/foo or /bar/baz)
  *      of the page to redirect to.  Usually only relevant for traditional HTTP requests,
  *      since if this was triggered from an AJAX or socket request, JSON should be sent instead.
  */
 
-module.exports[400] = function badRequest(validationErrors, redirectTo, req, res) {
-
-  /*
-   * NOTE: This function is Sails middleware-- that means that not only do `req` and `res`
-   * work just like their Express equivalents to handle HTTP requests, they also simulate
-   * the same interface for receiving socket messages.
-   */
+module.exports = function badRequest(validationErrors, redirectTo) {
+  
+  // Get access to `req`, `res`, `sails`
+  var req = this.req;
+  var res = this.res;
+  var sails = req._sails;
 
   var statusCode = 400;
 
@@ -69,5 +66,4 @@ module.exports[400] = function badRequest(validationErrors, redirectTo, req, res
 
   // If `redirectTo` was not specified, just respond w/ JSON
   return res.json(result, result.status);
-
 };
--- server/config/session.js
+++ server/config/session.js
@@ -1,7 +1,7 @@
 /**
  * Session
- * 
- * Sails session integration leans heavily on the great work already done by Express, but also unifies 
+ *
+ * Sails session integration leans heavily on the great work already done by Express, but also unifies
  * Socket.io with the Connect session store. It uses Connect's cookie parser to normalize configuration
  * differences between Express and Socket.io and hooks into Sails' middleware interpreter to allow you
  * to access and auto-save to `req.session` with Socket.io the same way you would with Express.
@@ -14,8 +14,16 @@ module.exports.session = {
 
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
-  // forcing them to log in again. 
-  secret: '7b8f2f6c0f52b17004918fe9b2b0fe7a'
+  // forcing them to log in again.
+  secret: 'bfe47e165810463a2518062f396e9505',
+
+
+  // Set the session cookie expire time
+  // The maxAge is set by milliseconds, the example below is for 24 hours
+  //
+  // cookie: {
+  //   maxAge: 24 * 60 * 60 * 1000
+  // }
 
 
   // In production, uncomment the following lines to set up a shared redis session store
