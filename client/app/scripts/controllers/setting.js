907fea2f94f00dd38adbe46464a605f3d3b78949
lyfeyaj <lyfeyaj@gmail.com>
2014-03-26 14:56:56 +0800

    add angular controller tag and setting

---
+++ client/app/scripts/controllers/setting.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('servantClientApp')
+  .controller('SettingCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/tag.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('servantClientApp')
+  .controller('TagCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
